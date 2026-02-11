996e9fbd8c1a3aab8fd31b66fd25ddb47eba9ad4
Originalxxx <originalgangstersdao@gmail.com>
2026-02-11 05:39:37 +0100

    Create GameActivity.java for the main game screen

---
+++ app/src/main/java/com/vessell/bubble/pop/GameActivity.java
@@ -0,0 +1,16 @@
+package com.vessell.bubble.pop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the main game layout
+        setContentView(R.layout.activity_game);
+    }
+
+    // Add additional game logic here
+}
