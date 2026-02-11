fb18f8aa8bf894777d698a8d9b53a4e2f4b3d4ce
Originalxxx <originalgangstersdao@gmail.com>
2026-02-11 05:38:27 +0100

    Create MainActivity.java with basic Android activity structure

---
+++ app/src/main/java/com/vessell/bubble/pop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vessell.bubble.pop;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
